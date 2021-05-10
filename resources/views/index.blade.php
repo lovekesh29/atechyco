88b36322906d29d21ef47be1191625db2c3b74c2
lovekesh <lovekesh.cypherdash@gmail.com>
2021-05-10 13:29:00 +0530

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,225 @@
+@extends('layouts.app')
+@include('layouts.header')
+@section('home')
+<section class="banner">
+        <div class="container">
+            <div class="col-lg-5 offset-lg-7 col-sm-12">
+                <div class="banner-content">
+                    <h1>Welcome To</h1>
+                    <span>Your Company</span>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section class="trending-courses main-page-section">
+        <div class="container">
+            <div class="section-heading text-center">
+                <h2>Trending Courses</h2>
+                <hr width="25%" align="center">
+            </div>
+            <p class="text-center course-content">some random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome
+                random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random text</p>
+
+            <div class="trending-slider row">
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section class="new-courses main-page-section">
+        <div class="container">
+            <div class="section-heading text-center">
+                <h2>Recent/New Courses</h2>
+                <hr width="25%" align="center">
+            </div>
+            <p class="text-center course-content">some random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome
+                random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random text</p>
+
+            <div class="trending-slider row">
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section class="suggested-courses main-page-section">
+        <div class="container suggested-slider">
+            <div class="section-heading text-center">
+                <h2>Suggested Courses</h2>
+                <hr width="25%" align="center">
+            </div>
+            <p class="text-center course-content">some random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome
+                random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random textsome random text</p>
+
+            <div class="trending-slider row">
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 crousal-element">
+                    <div class="card">
+                        <img src="images/javscript.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="contact-us main-page-section">
+        <div class="container contact-us-div">
+            <div class="section-heading text-center">
+                <h2>Contact Us</h2>
+            </div>
+            <p class="text-center course-content">Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some
+                Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text Some Random text </p>
+
+            <form class="contact-us-form">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="exampleFormControlInput1" class="form-label margin-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <select class="form-control" aria-label="Default select example">
+                            <option selected>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="exampleFormControlInput1" class="form-label margin-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <select class="form-select form-control" aria-label="Default select example">
+                            <option selected>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <button class="btn form-button float-right me-2" type="submit">Contact Us</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </section>
+@endsection
+@include('layouts.footer')
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,84 @@
+@section('footer')
+<footer class="main-page-section">
+        <div class="container subscribe-div">
+            <div class="footer-subscribe-section">
+                <div class="right-subscribe">
+                    <div class="right-subscribe-bg">
+                        <div class="row">
+                            <div class="col-lg-5 col-sm-12 subscribe-content">
+                                <h2>Subscribe To Our NewsLetter</h2>
+                                <p>Some Random Text Some Random Text Some Random Text Some Random Text </p>
+                            </div>
+                            <div class="col-lg-4 offset-lg-2 col-sm-12 subscribe-form">
+                                <form class="">
+                                    <div class="col-lg-12">
+                                        <input type="email" class="form-control subscribe-input" id="exampleFormControlInput1" placeholder="name@example.com">
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <button class="btn form-button subscribe-button me-2" type="submit">Subscribe</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+        <div class="container main-footer">
+            <div class="row">
+                <div class="col-lg-3">
+                    <h3>About Us</h3>
+                    <p>Some Random Text</p>
+                </div>
+                <div class="col-lg-3">
+                    <h3>Use Full Links</h3>
+                    <ul>
+                        <li>Link 1</li>
+                        <li>Link 2</li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <h3>Use Full Links</h3>
+                    <ul>
+                        <li>Link 1</li>
+                        <li>Link 2</li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <div class="phone d-flex">
+                        <i class="fas fa-phone-alt phone-icon"></i>
+                        <div class="phone-text">
+                            <p class="phone-number">9896788095</p>
+                            <p class="phone-number">9896788095</p>
+                        </div>
+                    </div>
+                    <div class="phone d-flex">
+                        <i class="fas fa-map-marker-alt phone-icon"></i>
+                        <div class="phone-text">
+                            <p class="phone-number">This is the sample address</p>
+                            <p class="phone-number">This is the sample address Line 2</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="float-left">Copyright &copy; 2021. Your CompanyName. All Rights Reserved</p>
+                    <span class="social-icons float-right">
+                        <i class="fab fa-facebook-square fa-lg"></i>
+                        <i class="fab fa-instagram-square fa-lg"></i>
+                        <i class="fab fa-twitter-square fa-lg"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
